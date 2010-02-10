2c85bd38732b24b7ce191cd0cd5c64a67e70ab75
Rajiv Dulepet <rdulepet@gmail.com>
2010-02-10 05:00:47 -0800

    added test R script to test instrumentation, in addition integrated instrumentation logic into job, and most importantly do instrumentation only if necessary, thus giving flexibility

---
+++ testing/test_instrumentation.r
@@ -0,0 +1,10 @@
+#<crdata_header/>
+#<crdata_title>This is brand new CRDATA run report</crdata_title>
+tmp <- as.data.frame(matrix(rnorm(100),ncol=10))
+summary(tmp)
+#<crdata_object>tmp</crdata_object>
+#<crdata_section/>
+#<crdata_image caption="Sample Distribution">
+plot(tmp)
+#</crdata_image>
+#<crdata_footer/>
