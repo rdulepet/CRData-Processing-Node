4763a3f91edd67a893e18e9be6ff6f6059e81f6f
Rajiv Dulepet <rdulepet@gmail.com>
2010-02-23 21:40:34 -0800

    fixed the carriage return test script

--- testing/crdatapnt/simpletest/noparam/carriage_return_test.r
+++ testing/crdatapnt/simpletest/noparam/carriage_return_test.r
@@ -1,39 +1,14 @@
-library("R2HTML")
-crdata_job_log <- file("job.log", open="wt")
-sink(crdata_job_log)
-crdata_target <- HTMLInitFile(getwd(), filename="index")
-tryCatch({
-source("/home/michael/crdatapn/temp/62da7647-5b6f-2ab0-2ad6-eedff9fbe61e/inc_62da7647-5b6f-2ab0-2ad6-eedff9fbe61e.r")
-HTML("Arbitrary Text 00", file=crdata_target)
+#<crdata_text>Arbitrary Text 00</crdata_text>
 
 x = c(0,1,2,3,4,5)
 
-HTML(x, file=crdata_target)
+
+#<crdata_object>x</crdata_object>
+
+#<crdata_image>
+plot(x)
+#</crdata_image>
 
 
-png(file.path(getwd(),"d6e2ade9-6aee-e155-8330-9660e7a3c0d3.png"))
-plot(x)
+#<crdata_text>Arbitrary Text 01</crdata_text>
 
-dev.off()
-HTMLInsertGraph("d6e2ade9-6aee-e155-8330-9660e7a3c0d3.png", file=crdata_target,caption="")
-
-HTML("Arbitrary Text 01", file=crdata_target)
-
-
-}, interrupt = function(ex) {
-print ("got exception: Failed Job");
- returnstatus="FAILED JOB, PLEASE CHECK LOG"; 
-HTML(returnstatus, file=crdata_target);
-print(ex);
-}, error = function(ex) {
-print ("got error: Failed Job");
- returnstatus="FAILED JOB, PLEASE CHECK LOG"; 
-HTML(returnstatus, file=crdata_target);
-print(ex);
-}, finally = {
-print("JOB ENDED");
-HTMLEndFile()
-sink()
-close(crdata_job_log)
-
-})
